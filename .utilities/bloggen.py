4a2941bfe2a252f9f978e914706fb7edfa20b886
neo <david@aizenberg.co.uk>
2023-01-05 03:45:17 +0200

    Update bloggen.py

--- .utilities/bloggen.py
+++ .utilities/bloggen.py
@@ -11,11 +11,6 @@ BLOG_PATH = "blog/"
 
 # FUNCTIONS
 
-# Load the blog from the blog directory and meta.json file
-
-# Load the blog entries from the blog directory
-
-
 def list_blog_entries() -> dict:
     blogDict = {}
     for blogItem in [a for a in os.listdir(BLOG_PATH) if os.path.isdir(BLOG_PATH + a)]:
@@ -25,24 +20,14 @@ def list_blog_entries() -> dict:
             blogDict[blogItem] = load_blog_entry_meta(blogItem)
     return blogDict
 
-# Load the blog entry meta data from the meta.json file
-
-
 def load_blog_entry_meta(blogEntry: str) -> dict:
     # parse the meta.json file
     with open(BLOG_PATH + blogEntry + '/meta.json', 'r') as metaFile:
         metaDict = json.load(metaFile)
         return metaDict
 
-
-# Generate the blog entry
-
-
 def generate_blog_entry(blogEntry: str) -> None:
     pass
-
-# Generate the feed
-
 
 def generate_feed(blogItems: list) -> None:
     fg = FeedGenerator()
